17aa416de6ec37fbc88642a2b888b6efd7432bb1
Namra Fanse <35749104+NamraOnPC@users.noreply.github.com>
2018-10-14 17:17:30 -0400

    Add files via upload

---
+++ workshop1/Ticket.java
@@ -0,0 +1,28 @@
+/**
+ * @author Namra Rupesh Fanse
+ *
+ */
+
+package jac444.wk1;
+
+import java.util.Date;
+
+import java.util.Scanner;
+
+public class Ticket {
+
+	public static int _draw[] = new int[3];
+
+	public int _playerDraw[] = new int[3];
+
+	Date date;
+
+	/*
+	 * public static void setDraw(int draw[]) {
+	 * 
+	 * System.arraycopy( draw , 0 , _draw , 0 , draw.length);
+	 * 
+	 * }
+	 */
+
+}
